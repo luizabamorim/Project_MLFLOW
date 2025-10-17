b090225ae127a0ea7ea878241b650da7c8e2224a
Luiza Amorim <46163704+luizabamorim@users.noreply.github.com>
2025-10-16 22:12:49 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLflow project for predicting credit card default (Kaggle: Default of Credit Card Clients Dataset).',
+    author='Luiza.Ferreira',
+    license='',
+)
